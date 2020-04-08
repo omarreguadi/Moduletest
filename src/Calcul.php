f6764ec5c53adb36263a9052af3d0c854a4c6a2f
omar <omarreguadi@gmail.com>
2020-04-08 17:05:54 +0200

    first commit

---
+++ src/Calcul.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App;
+
+class Calcul
+{
+    public function add($a, $b)
+    {
+        return $a + $b;
+    }
+
+    public function sub($a, $b)
+    {
+        return $a - $b;
+    }
+
+    public function mul($a, $b)
+    {
+        return $a * $b;
+    }
+
+    public function div($a, $b)
+    {
+        return $a/$b;
+    }
+}
---
+++ tests/CalculTest.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Tests;
+
+use App\Calcul;
+use PHPUnit\Framework\TestCase;
+
+final class CalculTest extends TestCase
+{
+    public function testAdd($a, $b)
+    {
+        $calculator = new Calcul();
+        $result = $calculator->add(20, 20);
+        $this->assertEquals(40, $result);
+    }
+
+    public function testDiv($a, $b)
+    {
+        $calculator = new Calcul();
+        $result = $calculator->div(40, 4);
+        $this->assertEquals(10, $result);
+    }
+
+    public function testMul($a, $b)
+    {
+        $calculator = new Calcul();
+        $result = $calculator->mul(50, 2);
+        $this->assertEquals(100, $result);
+    }
+
+    public function testSub($a, $b)
+    {
+        $calculator = new Calcul();
+        $result = $calculator->sub(100, 2);
+        $this->assertEquals(98, $result);
+    }
+}
